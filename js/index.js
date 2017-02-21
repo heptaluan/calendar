17578f0dbf5477413d43612a6428d31e4eac5aee
hanekaoru <vd2o@foxmail.com>
2017-02-21 11:46:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,64 @@
-$(function(){
-    var myDate = {
+function MyDate(box, year, month) { 
 
-        // 返回传入的 x年-x月 的第一天是星期几
-        setWeek: function (year, month) {
-            var newDay = new Date();
-            var days = [7, 1, 2, 3, 4, 5, 6];
+    // 获取当前月总共有多少天
+    var allDays = this.monthDay(year, month);
 
-            newDay.setFullYear(year);
-            newDay.setMonth(month - 1);
-            newDay.setDate(1);
+    // 获取当前月第一天星期几
+    var firstDay = this.weekDay(year, month);
+    
+    if (firstDay == 7) {
+        firstDay = 0;
+    }
 
-            return days[newDay.getDay()];
-        },
+    // 前置占位符
+    var placeholderSpan = "";
+    for (var i = 0; i < Number(firstDay) - 1; i++) {
+        placeholderSpan += '<span class="placeholderSpan"><i></i></span>';
+    }
 
-        // 返回传入的 x年-x月 的月份共有多少天
-        setMonth: function (year, month) {
-            var newDay = new Date();
+    // 生成日历
+    var day = "";
+    for (var i = 1; i <= allDays; i++) { 
+        day += '<span><i>' + i + '</i></span>'
+    }
 
-            newDay.setFullYear(year);
-            newDay.setMonth(month - 1);
+    box.innerHTML = placeholderSpan + day;
 
-            var everyMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+}
 
-            // 如果传入的 2月 是闰年的 2月
-            if (month == 2) {
-                var bissextile = [1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048];
+// 返回传入的 x年-x月 的第一天是星期几
+MyDate.prototype.weekDay = function (year, month) {
+    var newDay = new Date();
+    var days = [7, 1, 2, 3, 4, 5, 6];
 
-                for (let i = 0; i < bissextile.length; i++) {
-                    if (year == bissextile[i]) {
-                        everyMonth[1] = 29;
-                    }
-                }
+    newDay.setFullYear(year);
+    newDay.setMonth(month - 1);
+    newDay.setDate(1);
+
+    return days[newDay.getDay()];
+};
+
+// 返回传入的 x年-x月 的月份共有多少天
+MyDate.prototype.monthDay = function (year, month) {
+    var newDay = new Date();
+
+    newDay.setFullYear(year);
+    newDay.setMonth(month - 1);
+
+    var everyMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // 如果传入的 2月 是闰年的 2月
+    if (month == 2) {
+        var bissextile = [1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048];
+
+        for (let i = 0; i < bissextile.length; i++) {
+            if (year == bissextile[i]) {
+                everyMonth[1] = 29;
             }
-
-            return everyMonth[month - 1];
-        },
-
-        // 填充天数（生成日历）
-        createDate: function (box, year, month) {
-            // ...
         }
     }
-})
+
+    return everyMonth[month - 1];
+};
+
+
