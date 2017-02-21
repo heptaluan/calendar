ba1a9b362095cf2c51c2477b2865aaacc0c8bb26
hanekaoru <vd2o@foxmail.com>
2017-02-21 14:19:02 +0800

    wrapper functions

--- js/index.js
+++ js/index.js
@@ -1,28 +1,18 @@
-function MyDate(box, year, month) { 
+function MyDate(options) { 
 
-    // 获取当前月总共有多少天
-    var allDays = this.monthDay(year, month);
-
-    // 获取当前月第一天星期几
-    var firstDay = this.weekDay(year, month);
-    
-    if (firstDay == 7) {
-        firstDay = 0;
+    var defaults = {
+        box: document.querySelector("#myDate"),  // 日历容器
+        year: new Date().getFullYear(),  // 默认为系统年份
+        month: new Date().getMonth() + 1, // 默认为系统月份
+        calendars: 1  // 传入的日历个数，默认为 2
     }
 
-    // 前置占位符
-    var placeholderSpan = "";
-    for (var i = 0; i < Number(firstDay) - 1; i++) {
-        placeholderSpan += '<span class="placeholderSpan"><i></i></span>';
-    }
+    // 合并参数
+    var opts = $.extend({}, defaults, options);
 
-    // 生成日历
-    var day = "";
-    for (var i = 1; i <= allDays; i++) { 
-        day += '<span><i>' + i + '</i></span>'
-    }
+    // 初始化日历
+    this.createCal(opts.box, opts.year, opts.month, opts.calendars);
 
-    box.innerHTML = placeholderSpan + day;
 
 }
 
@@ -60,5 +50,78 @@ MyDate.prototype.monthDay = function (year, month) {
 
     return everyMonth[month - 1];
 };
+
+// 生成日历
+MyDate.prototype.createCal = function (box, year, month, calendars) {
+
+    // 单日历
+    if (calendars == 1) {
+        // 获取当前月总共有多少天
+        var allDays = this.monthDay(year, month);
+
+        // 获取当前月第一天星期几
+        var firstDay = this.weekDay(year, month);
+        
+        if (firstDay == 7) {
+            firstDay = 0;
+        }
+
+
+        // 前置占位符
+        var placeholderSpan = "";
+        for (var i = 0; i < Number(firstDay) - 1; i++) {
+            placeholderSpan += '<span class="placeholderSpan"><i></i></span>';
+        }
+
+        // 生成日历
+        var day = "";
+        for (var i = 1; i <= allDays; i++) { 
+            day += '<span><i>' + i + '</i></span>'
+        }
+
+        var myCalendar = '<div class="Mytime" id="first"><span id="prev">←</span><span id="next">→</span>' + 
+                '<div class="dateTitle"><em class="year">' + year + '</em> 年 <em class="month">' + month + '</em> 月</div>' + 
+                '<div class="dateList"><div class="week">' + 
+                '<span>一</span><span>二</span><span>三</span> <span>四</span><span>五</span><span>六</span><span>日</span></div>' + 
+                '<div class="day">' + placeholderSpan + day + '</div></div></div> ';
+                
+        box.innerHTML = myCalendar;
+    }
+
+    // 双日历
+    if (calendars == 2) {
+        // 获取当前月总共有多少天
+        var allDays = this.monthDay(year, month);
+
+        // 获取当前月第一天星期几
+        var firstDay = this.weekDay(year, month);
+        
+        if (firstDay == 7) {
+            firstDay = 0;
+        }
+
+
+        // 前置占位符
+        var placeholderSpan = "";
+        for (var i = 0; i < Number(firstDay) - 1; i++) {
+            placeholderSpan += '<span class="placeholderSpan"><i></i></span>';
+        }
+
+        // 生成日历
+        var day = "";
+        for (var i = 1; i <= allDays; i++) { 
+            day += '<span><i>' + i + '</i></span>'
+        }
+
+        var myCalendar = '<div class="Mytime" id="first">' + 
+                '<div class="dateTitle"><em class="year">' + year + '</em> 年 <em class="month">' + month + '</em> 月</div>' + 
+                '<div class="dateList"><div class="week">' + 
+                '<span>一</span><span>二</span><span>三</span> <span>四</span><span>五</span><span>六</span><span>日</span></div>' + 
+                '<div class="day">' + placeholderSpan + day + '</div></div></div> ';
+                
+        box.innerHTML = myCalendar;
+    }
+    
+}
 
 
